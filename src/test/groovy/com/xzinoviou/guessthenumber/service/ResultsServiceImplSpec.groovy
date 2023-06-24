02d646822fb9c5f67316b36505fc770818d4e3d6
Xenofon Zinoviou <xzinoviou@gmail.com>
2023-06-25 02:05:38 +0300

    add spock test for get result by game id happy path

--- src/test/groovy/com/xzinoviou/guessthenumber/service/ResultsServiceImplSpec.groovy
+++ src/test/groovy/com/xzinoviou/guessthenumber/service/ResultsServiceImplSpec.groovy
@@ -1,5 +1,8 @@
 package com.xzinoviou.guessthenumber.service
 
+import com.xzinoviou.guessthenumber.model.Game
+import com.xzinoviou.guessthenumber.model.GameStatus
+import com.xzinoviou.guessthenumber.model.Guess
 import spock.lang.Specification
 
 /**
@@ -24,5 +27,33 @@ class ResultsServiceImplSpec extends Specification {
         testClass = null
         playerService = null
         gameService = null
+    }
+
+    def "getGameResultsByGameId - when game id supplied then return a GameResultsDto for that game"() {
+        given: "a game id is supplied"
+        def gameId = 1
+        def playerId = 1
+        def game = new Game(
+                id: gameId,
+                playerId: playerId,
+                statusInfo: GameStatus.IN_PROGRESS,
+                totalScore: 2,
+                target: 44,
+                guesses: [new Guess(), new Guess()],
+                attempts: 2)
+
+        and: "a valid game"
+        gameService.getById(gameId) >> game
+
+        when: "a request by valid game id is sent"
+        def result = testClass.getGameResultsByGameId(gameId)
+
+        then: "a matching GameResultsDto is returned"
+        result.attempts == game.attempts
+        result.totalScore == game.totalScore
+        result.gameId == game.id
+        result.playerId == game.playerId
+        result.statusInfo.message == GameStatus.IN_PROGRESS.message
+        result.statusInfo.status == GameStatus.IN_PROGRESS.status
     }
 }
