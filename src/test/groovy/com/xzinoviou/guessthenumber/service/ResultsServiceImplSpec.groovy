b62b24f3ee16a54d9178fa2910b82945a299b29f
Xenofon Zinoviou <xzinoviou@gmail.com>
2023-06-25 02:49:23 +0300

    add spock test for get result by player id happy path

--- src/test/groovy/com/xzinoviou/guessthenumber/service/ResultsServiceImplSpec.groovy
+++ src/test/groovy/com/xzinoviou/guessthenumber/service/ResultsServiceImplSpec.groovy
@@ -3,7 +3,10 @@ package com.xzinoviou.guessthenumber.service
 import com.xzinoviou.guessthenumber.model.Game
 import com.xzinoviou.guessthenumber.model.GameStatus
 import com.xzinoviou.guessthenumber.model.Guess
+import com.xzinoviou.guessthenumber.model.Player
 import spock.lang.Specification
+
+import java.time.Instant
 
 /**
  * @author : Xenofon Zinoviou
@@ -55,5 +58,40 @@ class ResultsServiceImplSpec extends Specification {
         result.playerId == game.playerId
         result.statusInfo.message == GameStatus.IN_PROGRESS.message
         result.statusInfo.status == GameStatus.IN_PROGRESS.status
+    }
+
+    def "getPlayerResultsByGameId - when player id supplied then return a PlayerResultsDto for that player"() {
+        given: "a player id is supplied"
+        def playerId = 1
+        def player = new Player(
+                id: playerId,
+                firstName: "first_name",
+                lastName: "last_name",
+                dateOfBirth: Instant.now().toString(),
+                totalGames: 5,
+                history: [
+                        new Game(totalScore: 11, statusInfo: GameStatus.WON),
+                        new Game(totalScore: 3, statusInfo: GameStatus.LOST),
+                        new Game(totalScore: 10, statusInfo: GameStatus.WON),
+                        new Game(totalScore: 3, statusInfo: GameStatus.LOST),
+                        new Game(totalScore: 3, statusInfo: GameStatus.LOST)
+                ]
+        )
+
+        and: "a player matching player id"
+        playerService.getById(playerId) >> player
+
+        when: "a request by valid player id is sent"
+        def result = testClass.getPlayerResultsByPlayerId(playerId)
+
+        then: "a matching PlayerResultsDto is returned"
+        result.id == player.id
+        result.firstName == player.firstName
+        result.lastName == player.lastName
+        result.totalGames == player.totalGames
+        result.won == 2
+        result.lost == 3
+        result.totalScore == 30
+        result.avgWinRatio == 0.4
     }
 }
